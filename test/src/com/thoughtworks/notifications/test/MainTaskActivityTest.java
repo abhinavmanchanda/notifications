8f4c8e944e62b182c4e129a1ef22ac4a904cbc10
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2012-08-25 16:42:42 +0530

    Object/Mansi: Clean up, and formatting.

--- test/src/com/thoughtworks/notifications/test/MainTaskActivityTest.java
+++ test/src/com/thoughtworks/notifications/test/MainTaskActivityTest.java
@@ -1,44 +1,40 @@
 package com.thoughtworks.notifications.test;
-import com.thoughtworks.notifications.MainTaskActivity;
-import com.thoughtworks.notifications.R;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.thoughtworks.notifications.MainTaskActivity;
+import com.thoughtworks.notifications.R;
 import com.thoughtworks.notifications.test.stubs.DataRepositoryStub;
 import com.thoughtworks.notifications.test.utils.TestUtilities;
 
 
-public class MainTaskActivityTest extends ActivityInstrumentationTestCase2<MainTaskActivity>{
+public class MainTaskActivityTest extends ActivityInstrumentationTestCase2<MainTaskActivity> {
 
-	
-	private MainTaskActivity activity;
 
-	public MainTaskActivityTest() {
-		super(MainTaskActivity.class);
-	}
+    private MainTaskActivity activity;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		activity = this.getActivity();
+    public MainTaskActivityTest() {
+        super(MainTaskActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        activity = this.getActivity();
         activity.setDataRepository(new DataRepositoryStub(getInstrumentation().getTargetContext()));
-	}
-	
-	public void testButtonAddsItemToList() throws InterruptedException{
-		TestUtilities.setEditText(R.id.txtTaskTitle, "Notification!", activity, this);
-		Button button = (Button) activity.findViewById(R.id.btnAddTask);
-		TouchUtils.clickView(this, button);
-		ListView list = (ListView) activity.findViewById(android.R.id.list);
-		assertEquals(1, list.getChildCount());
-		assertEquals("Notification!", ((TextView)list.getChildAt(0)).getText());
-	}
-	
-		
+    }
+
+    public void testButtonAddsItemToList() throws InterruptedException {
+        TestUtilities.setEditText(R.id.txtTaskTitle, "Notification!", activity, this);
+        Button button = (Button) activity.findViewById(R.id.btnAddTask);
+        TouchUtils.clickView(this, button);
+        ListView list = (ListView) activity.findViewById(android.R.id.list);
+        assertEquals(1, list.getChildCount());
+        assertEquals("Notification!", ((TextView) list.getChildAt(0)).getText());
+    }
 
 
 }
--- test/src/com/thoughtworks/notifications/test/NewToDoActivityTest.java
+++ test/src/com/thoughtworks/notifications/test/NewToDoActivityTest.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.notifications.test;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
@@ -14,32 +13,32 @@ import static com.thoughtworks.notifications.NotificationsConstants.TITLE_KEY;
 @TargetApi(8)
 public class NewToDoActivityTest extends ActivityInstrumentationTestCase2<NewToDoActivity> {
 
-  private NewToDoActivity activity;
+    private NewToDoActivity activity;
 
-  public NewToDoActivityTest(){
-    super(NewToDoActivity.class);
-  }
+    public NewToDoActivityTest() {
+        super(NewToDoActivity.class);
+    }
 
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-    Intent intent = new Intent();
-    intent.putExtra(TITLE_KEY,"super title");
-    this.setActivityIntent(intent);
-    activity = this.getActivity();
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        Intent intent = new Intent();
+        intent.putExtra(TITLE_KEY, "super title");
+        this.setActivityIntent(intent);
+        activity = this.getActivity();
 
-  }
+    }
 
-  public void testPrepopulateTitle() throws Exception {
-    EditText editText = (EditText) activity.findViewById(R.id.todoText);
-    assertEquals("super title",editText.getText().toString());
-  }
-	
-  public void testShouldPrePopulateDescription() throws InterruptedException{
-		TestUtilities.setEditText(R.id.description, "This is a desc.", activity,this);
-		EditText title = (EditText) activity.findViewById(R.id.description);
-		assertEquals("This is a desc.", title.getText().toString());
-	}
-	
+    public void testPrepopulateTitle() throws Exception {
+        EditText editText = (EditText) activity.findViewById(R.id.todoText);
+        assertEquals("super title", editText.getText().toString());
+    }
+
+    public void testShouldPrePopulateDescription() throws InterruptedException {
+        TestUtilities.setEditText(R.id.description, "This is a desc.", activity, this);
+        EditText title = (EditText) activity.findViewById(R.id.description);
+        assertEquals("This is a desc.", title.getText().toString());
+    }
+
 }
 
